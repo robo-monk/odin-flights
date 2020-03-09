0cfb2140ebed441f08ddedba33ef0db295c70d54
robo-monk <filtap007@gmail.com>
2020-03-09 10:46:13 +0100

    add sendgrid for heroku deployment

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,14 @@
 # Load the Rails application.
 require_relative 'application'
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'infinite-ridge-41115.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWORD'],
+#   :domain => 'infinite-ridge-41115.herokuapp.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
 
 Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,22 +109,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  
+
    # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { :host => 'https://infinite-ridge-41115.herokuapp.com/' }
-
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
-    :address => 'smtp.sendgrid.net',
-    :domain => 'infinite-ridge-41115.herokuapp.com',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.default_url_options = { host: "https://infinite-ridge-41115.herokuapp.com/" }
 
 end
